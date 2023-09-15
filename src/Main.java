485f36d683eb422a9fad4ce8f5603d4cd2d6604a
Sergei <sk_developer@mail.ru>
2023-09-15 15:08:38 +0300

    Task9 выполнен отчет и рефлексия

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> l1 = new ArrayList<>();
+        l1.add(42);
+        List<? extends Number> l2 = l1;
+
+        List<Number> l3 = new ArrayList<>();
+        List<? super Double> l4 = l3;
+    }
+}
---
+++ src/Task9/General.java
@@ -0,0 +1,81 @@
+package Task9;
+
+import java.io.*;
+import java.lang.reflect.Field;
+
+public abstract class General implements Serializable, Cloneable {
+    //копирование объекта
+    public General deepCopy() {
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(this);
+
+            ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+            ObjectInputStream ois = new ObjectInputStream(bis);
+            return (General) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //клонирование объекта
+    public General clone() {
+        try {
+            return (General) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //сравнение объектов
+    protected boolean equals(General other) {
+        return this.equals(other);
+    }
+
+    //сериализация
+    public byte[] serialize() throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(this);
+        oos.flush();
+        return bos.toByteArray();
+    }
+
+    //десериализация
+    public static General deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        return (General) ois.readObject();
+    }
+
+    //печать в удобном виде
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Field[] fields = this.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            try {
+                field.setAccessible(true);
+                sb.append(field.getName()).append(": ").append(field.get(this)).append("\n");
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
+
+    }
+
+    protected boolean isGeneralType(Class<?> someClass){
+        return this.getClass().equals(someClass) || someClass.isInstance(this);
+    }
+
+    protected Class<?> realTypeOf(){
+        return this.getClass();
+    };
+}
+
+class Any extends General {
+
+}
+
